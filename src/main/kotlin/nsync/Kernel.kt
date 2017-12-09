4134b3568a9f1ebe92b607745204193795c88504
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2017-12-09 17:36:44 -0500

    Unify Loader and Facade - Kernel.kt file

--- src/main/kotlin/nsync/Kernel.kt
+++ src/main/kotlin/nsync/Kernel.kt
@@ -7,15 +7,50 @@ import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.Context
 import ch.qos.logback.core.FileAppender
+import commons.*
+import kotlinx.coroutines.experimental.runBlocking
 import mu.KLogging
+import mu.KotlinLogging
 import nsync.analyzer.LocalFileWatchServer
 import nsync.metadata.MetadataServer
 import nsync.storage.StorageServer
 import nsync.synchronization.SyncServer
 import org.slf4j.LoggerFactory
-import commons.AsyncBus
-import commons.Configuration
+import java.net.URI
 import java.nio.file.Path
+import kotlin.reflect.KClass
+import kotlin.system.measureTimeMillis
+
+const val name = "NSync"
+const val version = "1.0.0-devel"
+
+class KernelFacade(
+        private val bus: SignalBus,
+        private val metadata: MetadataServer
+) {
+    private val logger = KotlinLogging.logger {}
+
+    fun start() = runBlocking {
+        logger.info { "Starting $name kernel" }
+        val running = measureTimeMillis {
+            bus.join()
+        }
+
+        logger.info { "$name kernel has stopped (running time: $running ms)" }
+    }
+
+    suspend fun stop() =
+            bus.publish(Stop())
+
+    fun <E, T : Signal<E>> connect(eventKlass: KClass<T>): Connection<E, T> {
+        return this.bus.connect(eventKlass)
+    }
+
+    // TODO move it to iteractors
+    suspend fun addFS(localUri: String, remoteUri: String) =
+            bus.publish(AddFS(FS(localFolder = URI(localUri), remoteFolder = URI(remoteUri))))
+
+}
 
 /**
  * This class is responsible by bootstrapping the app.
